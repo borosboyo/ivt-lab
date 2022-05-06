b7162f7f6f3ba31acaa30bc3f6dd8fcdf4b1a7ca
borosboyo <borosgergo00@gmail.com>
2022-05-06 09:04:07 +0200

    done

--- src/test/java/hu/bme/mit/spaceship/GT4500Test.java
+++ src/test/java/hu/bme/mit/spaceship/GT4500Test.java
@@ -9,31 +9,45 @@ import static org.mockito.Mockito.*;
 public class GT4500Test {
 
   private GT4500 ship;
+  private TorpedoStore mockPrimaryTs;
+  private TorpedoStore mockSecondaryTs;
+
 
   @BeforeEach
   public void init(){
-    this.ship = new GT4500();
+    mockPrimaryTs = mock(TorpedoStore.class);
+    mockSecondaryTs = mock(TorpedoStore.class);
+    this.ship = new GT4500(mockPrimaryTs, mockSecondaryTs);
   }
 
   @Test
   public void fireTorpedo_Single_Success(){
     // Arrange
+    when(mockPrimaryTs.fire(1)).thenReturn(true);
+    when(mockSecondaryTs.fire(1)).thenReturn(true);
 
     // Act
     boolean result = ship.fireTorpedo(FiringMode.SINGLE);
 
+  
     // Assert
+    verify(mockPrimaryTs, times(1)).fire(1);
+    //verify(mockSecondaryTs, times(1)).fire(1);
     assertEquals(true, result);
   }
 
   @Test
   public void fireTorpedo_All_Success(){
     // Arrange
+    when(mockPrimaryTs.fire(10)).thenReturn(true);
+    when(mockSecondaryTs.fire(10)).thenReturn(true);
 
     // Act
     boolean result = ship.fireTorpedo(FiringMode.ALL);
 
     // Assert
+    verify(mockPrimaryTs, times(1)).fire(10);
+    verify(mockSecondaryTs, times(1)).fire(10);
     assertEquals(true, result);
   }
 
